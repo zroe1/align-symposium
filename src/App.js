970b4cc966456a5d53426b9ef2df0d6d836c9674
zephyroe <114773939+zroe1@users.noreply.github.com>
2024-08-10 15:14:12 -0500

    about the event section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <About></About>
     </div>
   );
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,28 @@
+import forum from "./forum.jpg";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <img src={forum} alt="Forum" className="about-img" />
+      <div className="about-text-div">
+        <h2 className="about-title">About the event</h2>
+        <div className="marroon-divider"></div>
+        <p className="about-text">
+          The Chicago Symposium on Transformative AI is an exclusive, two-day event taking place on
+          November 9-10, 2024, at the prestigious David Rubenstein Forum at the University of
+          Chicago. This carefully curated symposium brings together 30-40 of the most promising
+          undergraduate students in the field of AI safety. Nestled in the heart of Chicago's
+          intellectual hub, participants will engage in rigorous discussions, attend expert-led
+          talks, and collaborate on refining their models of AI's transformative potential. Our
+          venue offers a perfect blend of cutting-edge facilities and an inspiring academic
+          atmosphere, setting the stage for breakthrough insights and meaningful connections. Join
+          us for this unique opportunity to shape the future of AI safety alongside peers and
+          industry leaders.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
